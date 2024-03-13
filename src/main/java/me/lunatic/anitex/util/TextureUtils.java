9c9e9ab839e74c0a3fdd73e4c3fe9b53a811cd79
Luna <kiana@chloedev.de>
2024-03-13 17:23:13 +0100

    - Remove leftover files - Add Support for loading Frames from the filesystem

--- src/main/java/me/lunatic/anitex/util/TextureUtils.java
+++ src/main/java/me/lunatic/anitex/util/TextureUtils.java
@@ -3,12 +3,16 @@ package me.lunatic.anitex.util;
 import me.lunatic.anitex.tex.Frame;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.texture.NativeImageBackedTexture;
 import net.minecraft.resource.Resource;
 import net.minecraft.util.Identifier;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -27,8 +31,26 @@ public class TextureUtils {
                     try (NativeImage img = NativeImage.read(in)) {
                         list.add(new Frame(id, img.getWidth(), img.getHeight()));
                     }
-                } catch (IOException e) {
+                } catch (IOException ignored) {
+                }
+            }
+        }
+        return list.stream().sorted(new FileNameComparator()).toList();
+    }
 
+    public static List<Frame> getFrameFiles(File directory) {
+        List<Frame> list = new ArrayList<>();
+        if (directory != null && directory.exists() && directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                files = Arrays.stream(files).filter(file -> file.isFile() && file.getName().endsWith(".png")).toArray(File[]::new);
+                for (File file : files) {
+                    try (FileInputStream in = new FileInputStream(file)) {
+                        try (NativeImage img = NativeImage.read(in)) {
+                            list.add(new Frame(MinecraftClient.getInstance().getTextureManager().registerDynamicTexture("tex_", new NativeImageBackedTexture(img)), img.getWidth(), img.getHeight()));
+                        }
+                    } catch (IOException ignored) {
+                    }
                 }
             }
         }
--- src/test/java/me/lunatic/test/mixin/MixinOptionsScreen.java
+++ src/test/java/me/lunatic/test/mixin/MixinOptionsScreen.java
@@ -13,11 +13,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.io.File;
+
 @Mixin(OptionsScreen.class)
 public class MixinOptionsScreen extends Screen {
 
     @Unique
     private AnimatedTexture sampleTexture;
+    @Unique
+    private AnimatedTexture sampleTextureFromFile;
 
     protected MixinOptionsScreen(Text title) {
         super(title);
@@ -25,11 +29,16 @@ public class MixinOptionsScreen extends Screen {
 
     @Inject(method = "init", at = @At("TAIL"))
     public void init(CallbackInfo ci) {
+        // BIG NOTE: You almost never want to create textures more than once. Depending on the amount of frames, this may result in a lot of memory-consumption.
+        // This is just as an example. Rather register them once on startup, and inside a reload listener (https://fabricmc.net/wiki/tutorial:custom_resources)
         // Here we create a new AnimatedTexture Object. Also provide the delay in milliseconds.
         this.sampleTexture = new AnimatedTexture(1000);
         // If you want it to actually render anything, you need to also load in some frames.
         // NOTE: getFrameResources will load all frames from a directory, in alphabetical order.
         this.sampleTexture.load(TextureUtils.getFrameResources(new Identifier("test", "textures/test")));
+        // You can also load textures from actual files instead of assets
+        this.sampleTextureFromFile = new AnimatedTexture(1000);
+        this.sampleTextureFromFile.load(TextureUtils.getFrameFiles(new File("config/test/")));
     }
 
     @Override
@@ -41,6 +50,6 @@ public class MixinOptionsScreen extends Screen {
         // x/y - the top-left position of where to render the texture
         // width/height - the width and height to use when rendering the texture. NOT THE ACTUAL TEXTURE FILE DIMENSIONS!
         // delta - the millisecond-difference between the current and last frame. Same as the context, this is provided by most mc rendering methods.
-        this.sampleTexture.render(context, 0, 0, this.width, this.height, delta);
+        this.sampleTextureFromFile.render(context, 0, 0, this.width, this.height, delta);
     }
 }
